39acdeecc7348c0f94a0f3d5c227d3407b1fa663
unknown <morten@.hih.dk>
2010-10-06 22:42:06 +0200

    added lcd and swim files, compiles

---
+++ lpc2478_lcd_swim/platform/framework.h
@@ -0,0 +1,19 @@
+/******************************************************************************
+ *
+ * Copyright:
+ *    (C) 2000 - 2005 Embedded Artists AB
+ *
+ * Description:
+ *    Framework for ARM7 processor
+ *
+ *****************************************************************************/
+
+
+#ifndef _framework_h_
+#define _framework_h_
+
+#include "config.h"
+
+void lowLevelInit(void);
+
+#endif
